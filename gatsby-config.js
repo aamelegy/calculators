69d3f16916b4e744cdad2731c7261ed10e4cf672
Ahmed Melegy <amelegy@fb.com>
2020-01-04 00:32:34 +0000

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,24 +30,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "UA-155271009-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
