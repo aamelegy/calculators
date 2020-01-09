bdfa00d44589af17da23ec58e976bc0d19b49318
Ahmed Melegy <amelegy@fb.com>
2020-01-09 00:40:35 +0000

    tabs

--- src/components/volume_calculators.js
+++ src/components/volume_calculators.js
@@ -3,6 +3,8 @@ import RectangularVolumeCalculator from "../components/rectangual_volume_calcula
 import CylinderVolumeCalculator from "../components/cylinder_volume_calculator"
 import CubeVolumeCalculator from "../components/cube_volume_calculator"
 import Flexbox from "flexbox-react"
+import Tabs from "react-bootstrap/Tabs"
+import Tab from "react-bootstrap/Tab"
 
 class VolumeCalculators extends React.Component {
   constructor(props) {
@@ -11,22 +13,17 @@ class VolumeCalculators extends React.Component {
 
   render() {
     return (
-      <Flexbox
-        style={{ margin: 3 }}
-        flexGrow={1}
-        flexDirection={"row"}
-        flexWrap="wrap"
-      >
-        <Flexbox flex={1} justifyContent="center" margin="3px">
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <Tab eventKey="home" title="Cuboid Volume Calculator">
           <RectangularVolumeCalculator />
-        </Flexbox>
-        <Flexbox flex={1} justifyContent="center" margin="3px">
+        </Tab>
+        <Tab eventKey="profile" title="Cylinder Volume Calculator">
           <CylinderVolumeCalculator />
-        </Flexbox>
-        <Flexbox flex={1} justifyContent="center" margin="3px">
+        </Tab>
+        <Tab eventKey="contact" title="Cube Volume Calculator">
           <CubeVolumeCalculator />
-        </Flexbox>
-      </Flexbox>
+        </Tab>
+      </Tabs>
     )
   }
 }
