6dc06a590820d3e269f4345434eeb45d57c092f0
Ahmed Melegy <amelegy@fb.com>
2020-01-10 23:03:07 +0000

    window

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import VolumeCalculators from "../components/volume_calculators"
 import Flexbox from "flexbox-react"
 import { Helmet } from "react-helmet"
 import ReactGA from "react-ga"
-ReactGA.initialize("UA-155271009-1")
-ReactGA.pageview(window.location.pathname + window.location.search)
 
 //export PATH=${HOME}/.brew/bin:${PATH}
 class Main extends React.Component {
@@ -14,6 +12,8 @@ class Main extends React.Component {
   }
 
   render() {
+    ReactGA.initialize("UA-155271009-1")
+    ReactGA.pageview(window.location.pathname + window.location.search)
     return (
       <Flexbox height="100vh" flexDirection="column" alignItems="center">
         <div className="application">
