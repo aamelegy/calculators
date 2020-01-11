a28f873da2ff6334bd855ed598951ce0d235632b
Ahmed Melegy <amelegy@fb.com>
2020-01-11 00:06:52 +0000

    contact us

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import VolumeCalculators from "../components/volume_calculators"
 import Flexbox from "flexbox-react"
 import { Helmet } from "react-helmet"
 import ReactGA from "react-ga"
+import Button from "react-bootstrap/Button"
 ReactGA.initialize("UA-155271009-1")
 
 //export PATH=${HOME}/.brew/bin:${PATH}
 class Main extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { contactFromVisible: false }
   }
 
   render() {
@@ -42,6 +44,36 @@ class Main extends React.Component {
         </Flexbox>
 
         <VolumeCalculators />
+
+        <Button
+          variant="primary"
+          onClick={() => {
+            this.setState(prevState => ({
+              contactFromVisible: !prevState.contactFromVisible,
+            }))
+          }}
+        >
+          Something is not working or missing?{" "}
+        </Button>
+        {this.state.contactFromVisible ? (
+          <form
+            method="post"
+            action="https://getform.io/f/5e20d74d-922a-4102-a40d-bea225b8747e"
+          >
+            <Flexbox flexDirection="column">
+              <input type="hidden" name="bot-field" />
+              <label>
+                Something is not working or missing? Let us know. We'll do our
+                best to fix it
+              </label>
+              <label>Email</label>
+              <input type="email" name="email" />
+              <label>What is broken or missing?</label>
+              <textarea name="message" id="message" rows="1" />
+              <button type="submit">Send</button>
+            </Flexbox>
+          </form>
+        ) : null}
       </Flexbox>
     )
   }
